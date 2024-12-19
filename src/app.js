e9e67ae65b7f4c9290356eff97e6d4f77b3e20ce
RGuy2025 <r.s.guyett@gmail.com>
2024-12-19 09:45:32 -0500

    adding axios to the app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  
+}
+
+
+
+
+function searchCity(city) {
+  let apiKey = "5912o91beb33d634bfd91ta0a18fa0bd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let currentCityElement = document.querySelector("#current-city");
   currentCityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector(".search-form");
